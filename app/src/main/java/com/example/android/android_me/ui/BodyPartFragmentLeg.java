e76986ac83fc674ad535933104b65ced49456641
Anga <angakoko@gmail.com>
2018-01-24 22:40:05 +0100

    Create Class BodyPartFragmentLegs to inflate leg fragment

---
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragmentLeg.java
@@ -0,0 +1,10 @@
+package com.example.android.android_me.ui;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by ANGA on 1/24/2018.
+ */
+
+public class BodyPartFragmentLeg extends Fragment {
+}
